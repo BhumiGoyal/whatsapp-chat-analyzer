aa8852b799ee1d44b2ece9ee057f3f3e4857ff20
BhumiGoyal <goyalbhumi5@gmail.com>
2024-04-20 16:59:03 +0530

    project deployment

--- helper.py
+++ helper.py
@@ -3,7 +3,6 @@ extract = URLExtract()
 from wordcloud import WordCloud
 import pandas as pd
 from collections import Counter
-import emoji
 
 def fetch_stats(selected_user, df):
     if selected_user != "Overall":
